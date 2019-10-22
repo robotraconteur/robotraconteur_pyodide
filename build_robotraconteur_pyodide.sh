68e4bf1ef4a6b496db4ac47d8cf224581cbde9d3
John Wason <wason@wasontech.com>
2019-10-21 22:35:44 -0400

    Add patching to build_robotraconteur_pyodide.sh

--- build_robotraconteur_pyodide.sh
+++ build_robotraconteur_pyodide.sh
@@ -21,8 +21,8 @@ BOOST_LIBS_COMMA=$(echo $BOOST_LIBS | sed -e 's/[[:space:]]/,/g')
 
 source $PYODIDE_ROOT/emsdk/emsdk/emsdk_env.sh
 
-SIDE_C_FLAGS="-s DISABLE_EXCEPTION_CATCHING=0 -fpic"
-SIDE_LDFLAGS="-s \"BINARYEN_METHOD='native-wasm'\" -Werror -s EMULATED_FUNCTION_POINTERS=1 -s EMULATE_FUNCTION_POINTER_CASTS=1 -s SIDE_MODULE=1 -s WASM=1 -s \"BINARYEN_TRAP_MODE='clamp'\" --memory-init-file 0 -s LINKABLE=1 -s EXPORT_ALL=1 -s DISABLE_EXCEPTION_CATCHING=0"
+SIDE_C_FLAGS="-s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=1 -fpic"
+SIDE_LDFLAGS="-s \"BINARYEN_METHOD='native-wasm'\" -Werror -s EMULATED_FUNCTION_POINTERS=1 -s EMULATE_FUNCTION_POINTER_CASTS=1 -s SIDE_MODULE=1 -s WASM=1 -s \"BINARYEN_TRAP_MODE='clamp'\" --memory-init-file 0 -s LINKABLE=1 -s EXPORT_ALL=1 -s DISABLE_EXCEPTION_CATCHING=0 -s EXCEPTION_DEBUG=1"
 
 mkdir -p build_boost
 if [ ! -s $BOOST_TARBALL ]; then
@@ -60,9 +60,10 @@ fi
 -DCMAKE_SHARED_LINKER_FLAGS="$SIDE_LDFLAGS" \
 -DCMAKE_MODULE_LINKER_FLAGS="$SIDE_LDFLAGS" \
 -DCMAKE_CXX_FLAGS="$SIDE_C_FLAGS" \
+-DCMAKE_C_FLAGS="$SIDE_C_FLAGS"
  )
 
-( cd $CURRENT_DIR/build && make )
+( cd $CURRENT_DIR/build && make VERBOSE=1 )
 
 if [ -s $CURRENT_DIR/build/out/Python/RobotRaconteur/_RobotRaconteurPython.wasm ]; then
     mv $CURRENT_DIR/build/out/Python/RobotRaconteur/_RobotRaconteurPython.wasm $CURRENT_DIR/build/out/Python/RobotRaconteur/_RobotRaconteurPython.so
@@ -79,5 +80,29 @@ $CURRENT_DIR/build/out/python_install@ \
 cp -v $CURRENT_DIR/build/out/robotraconteur.data $PYODIDE_ROOT/build/
 cp -v $CURRENT_DIR/build/out/robotraconteur.js $PYODIDE_ROOT/build/
 
+# Add robotraconteur to packages.json in $PYODIDE_ROOT/build
+python3 << END
+import json
+with open('$PYODIDE_ROOT/build/packages.json', 'r') as f:
+    j = json.load(f)
+if 'robotraconteur' in j['dependencies']:
+    print("packages.json already updated")
+    exit(0)
+j['dependencies']['robotraconteur'] = ['numpy']
+j['import_name_to_package_name']['RobotRaconteur'] = 'robotraconteur'
+with open('$PYODIDE_ROOT/build/packages.json', 'w') as f:
+	json.dump(j,f)
+print("Updated packages.json")
+END
+
 # See https://github.com/iodide-project/pyodide/issues/507 for patch involving bug in core pyodide software
-# Also add robotraconteur to packages.json in $PYODIDE_ROOT/build
+python3 << END
+before='function(){return Module[prop].apply(null,arguments)}'
+after='function(){}'
+with open('$PYODIDE_ROOT/build/pyodide.asm.js', 'r') as f:
+    s=f.read()
+s=s.replace(before,after)
+with open('$PYODIDE_ROOT/build/pyodide.asm.js.new', 'w') as f:
+    f.write(s)
+
+END
